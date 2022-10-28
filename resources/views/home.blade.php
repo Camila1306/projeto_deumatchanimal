593bc34a189f34594dc6f9f180e586198dd748bd
Camila Cordeiro da Rocha <camilaa7x@gmail.com>
2022-10-28 17:19:02 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-title">
+                        <h3>De acordo com a Organização Mundial de Saúde(OMS), somente no Brasil, há cerca de 30 milhões de <br>animais abandonados,sendo aproximadamente 20 milhões de cães e 10 milhões de gatos</h3>
+                </div>
+            </div>
+            <div class="card">    
+                <div class="card-text">
+                    <p>A maioria dos abandonos são devido:
+                      <ul>
+                          <li>problemas comportamentais dos animais</li>
+                          <li>Falta de espaço</li>
+                          <li>Estilo de vida dos proprietários</li>
+                          <li>Falta de informação sobre responsabilidades e custos gerados pela guarda do animal</li>
+                      </ul>
+                     </p>
+                </div>             
+            </div>
+            <div class="card">
+                <div class="card-text">
+                    <h4>Antes de adotar saiba...</h4>
+                    <p>Animais precisam de atenção, espaço, alimento próprio para consumo, água limpa, ir ao médico periódicamente e ao adoecerem, e remédios</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
