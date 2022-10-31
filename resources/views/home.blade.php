97969622ea5c3bdceca7f595127b27ade2add23b
Camila Cordeiro da Rocha <camilaa7x@gmail.com>
2022-10-31 18:17:18 -0300

    Atualização home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,28 +4,65 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-title">
-                        <h3>De acordo com a Organização Mundial de Saúde(OMS), somente no Brasil, há cerca de 30 milhões de <br>animais abandonados,sendo aproximadamente 20 milhões de cães e 10 milhões de gatos</h3>
+            <div class="card border-danger">
+                <div class="card-headers">
+                    <h3>De acordo com a Organização Mundial de Saúde(OMS), somente no Brasil, há cerca de 30 milhões de <br>animais abandonados,sendo aproximadamente 20 milhões de cães e 10 milhões de gatos</h3>
                 </div>
             </div>
-            <div class="card">    
+            <div class="card border-danger">
                 <div class="card-text">
                     <p>A maioria dos abandonos são devido:
-                      <ul>
-                          <li>problemas comportamentais dos animais</li>
-                          <li>Falta de espaço</li>
-                          <li>Estilo de vida dos proprietários</li>
-                          <li>Falta de informação sobre responsabilidades e custos gerados pela guarda do animal</li>
-                      </ul>
-                     </p>
-                </div>             
+                    <ul>
+                        <li>problemas comportamentais dos animais</li>
+                        <li>Falta de espaço</li>
+                        <li>Estilo de vida dos proprietários</li>
+                        <li>Falta de informação sobre responsabilidades e custos gerados pela guarda do animal</li>
+                    </ul>
+                    </p>
+                </div>
             </div>
-            <div class="card">
+            <div class="card border-warning">
                 <div class="card-text">
                     <h4>Antes de adotar saiba...</h4>
                     <p>Animais precisam de atenção, espaço, alimento próprio para consumo, água limpa, ir ao médico periódicamente e ao adoecerem, e remédios</p>
-                    
+                </div>
+            </div>
+            <div class="card border-success">
+                <div class="card-text">
+                    Cachorros de porte pequeno consomem cerca de reais em ração por mês. Já os de porte médio, 70 reais por mês, enquanto que o de porte grande consomem 124 reais por mês.<br>
+                    Os gatos pequenos consomem cerca de 33 reais por mês de ração, enquanto os grande consomem cerca de 47 reais por mês. <br>
+
+                    Animais até um ano devem consumir ração de filhotes, de 1 a 7 anos a ração de adulto (se for cadastrado, opte pela ração de castrados), e acima de 7 anos utilize ração Senior.<br>
+
+                    Antes de comprar a ração, procure a orientação do médico veterinário do seu pet para que ele possa ter uma boa alimentação. A quantidade indicada por peso está em uma tabela na embalagem da ração, não se esqueça de observar isso. Lembre-se de variar na alimentação, dando frutas, carnes das quais eles possam consumir.
+
+                </div>
+            </div>
+            <div class="card border-info">
+                <div class="card-text">
+                    Remédios e vacinas:
+                    Cachorros que não tomaram as 3 primeiras doses da vacina devem ser levados ao veterinário para aplicar as 3 doses iniciais a cada 1 mês e a vacina da raiva. Após isso, a cada um ano reforce as vacinas.<br>
+                    Gatos precisam tomar 2 doses da vacina, uma a cada mês, e uma da raiva. Após isso, a cada ano reforce as vacinas.<br>
+                    É importante fazer a utilização de remédios antiparasitários e antipulgas em seu amigo, isso ajudara a manter o animal e ambiente limpo e saudável.
+                </div>
+            </div>
+            <div class="card border-warning">
+                <div class="card-text">
+                    <ul>
+                        <li>Faça diariamente exercícios mentais e físicos com seu animal</li>
+                        <li>Reserve um tempo toda a semana para lhe dar banho ou leva-lo no petshop</li>
+                        <li>Deixe sempre disponível água fresca e brinquedos</li>
+                        <li>Idas periódicas ao veterinário são muito importantes para manter a saúde do seu companheiro</li>
+                        <li>Lembre-se de sempre ter uma reserva para imprevistos</li>
+                        <li>Em caso de viajem: Leve seu companheiro se for possível, caso contrário, procure alguém que possa cuidar dele enquanto estiver fora</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="card border-success">
+                <div class="card-text">
+                    Animais sentem frio, sentem dor, fome, sede, e ficam doentes tanto quanto nós. <br>
+                    Antes de adotá-lo, se planeje financeiramente, tenha espaço disponível, e tempo para cuidados. <br>
+                    São como crianças e se não serem ensinados vão continuar a arrancar suas roupas do varal, vão roer seu chinelo, vão estagar seu sofá. Tenha paciência, amor e ensine com calma, eles são espertos, irão aprender.
                 </div>
             </div>
         </div>
